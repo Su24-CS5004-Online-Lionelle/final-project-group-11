de55a88ea5190fafca4cb400c347cf58c847196b
Xing Liu <liu.xing4@northeastern.edu>
2024-07-22 21:48:06 -0400

    added javadoc to FreeGameItem.java

--- bin/main/README.md
+++ bin/main/README.md
@@ -1,4 +1,3 @@
-# Java Src
+# Resources
 
-Main java source files - you do not have to have them in the 'student' package, but you will
-still want to consider other packages as it helps your project layout. 
+Default location for resources, should you need them. 
--- src/main/java/student/model/FreeGameItem.java
+++ src/main/java/student/model/FreeGameItem.java
@@ -1,19 +1,58 @@
 package student.model;
 import java.util.Objects;
 
+/**
+ * A class represents a free game item with various attributes such as title, genre, platform, etc.
+ */
 public class FreeGameItem {
+    /** The unique identifier of the game item. */
     private final int id;
+    
+    /** The title of the game. */
     private final String title;
+    
+    /** The URL of the game's thumbnail image. */
     private final String thumbnail;
+    
+    /** A brief description of the game. */
     private final String shortDescription;
+    
+    /** The URL to access the game. */
     private final String gameUrl;
+    
+    /** The genre of the game. */
     private final String genre;
+    
+    /** The platform on which the game can be played. */
     private final String platform;
+    
+    /** The publisher of the game. */
     private final String publisher;
+    
+    /** The developer of the game. */
     private final String developer;
+    
+    /** The release date of the game. */
     private final String releaseDate;
+    
+    /** The URL to the game's profile on the FreeToGame website. */
     private final String freetogameProfileUrl;
 
+    /**
+     * Constructs a new FreeGameItem with the specified details.
+     *
+     * @param id                  the unique identifier of the game item
+     * @param title               the title of the game
+     * @param thumbnail           the URL of the game's thumbnail image
+     * @param shortDescription    a brief description of the game
+     * @param gameUrl             the URL to access the game
+     * @param genre               the genre of the game
+     * @param platform            the platform on which the game can be played
+     * @param publisher           the publisher of the game
+     * @param developer           the developer of the game
+     * @param releaseDate         the release date of the game
+     * @param freetogameProfileUrl the URL to the game's profile on the FreeToGame website
+     */
     public FreeGameItem(int id, String title, String thumbnail, String shortDescription, String gameUrl,
                         String genre, String platform, String publisher, String developer, String releaseDate,
                         String freetogameProfileUrl) {
@@ -30,16 +69,59 @@ public class FreeGameItem {
         this.freetogameProfileUrl = freetogameProfileUrl;
     }
 
+    /**
+     * @return the unique identifier of the game item
+     */
     public int getId() { return id; }
+
+    /**
+     * @return the title of the game
+     */
     public String getTitle() { return title; }
+
+    /**
+     * @return the URL of the game's thumbnail image
+     */
     public String getThumbnail() { return thumbnail; }
+
+    /**
+     * @return a brief description of the game
+     */
     public String getShortDescription() { return shortDescription; }
+
+    /**
+     * @return the URL to access the game
+     */
     public String getGameUrl() { return gameUrl; }
+
+    /**
+     * @return the genre of the game
+     */
     public String getGenre() { return genre; }
+
+    /**
+     * @return the platform on which the game can be played
+     */
     public String getPlatform() { return platform; }
+
+    /**
+     * @return the publisher of the game
+     */
     public String getPublisher() { return publisher; }
+
+    /**
+     * @return the developer of the game
+     */
     public String getDeveloper() { return developer; }
+
+    /**
+     * @return the release date of the game
+     */
     public String getReleaseDate() { return releaseDate; }
+
+    /**
+     * @return the URL to the game's profile on the FreeToGame website
+     */
     public String getFreetogameProfileUrl() { return freetogameProfileUrl; }
 
     @Override
