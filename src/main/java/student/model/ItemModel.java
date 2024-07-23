2b460946f06182a6bc032a74f4eb42a23f19985c
Xing Liu <liu.xing4@northeastern.edu>
2024-07-23 00:11:51 -0400

    - updated 'org.json:json:20230227' to build.gradle - created a freegamerecords.json to have the local database - created an interface ItemModel.java - created an implementation ItemModelImpl.java - updated searchGameByName(String gameName) in NetUtils.java to retrieve a game info by using the game name via api searching

---
+++ src/main/java/student/model/ItemModel.java
@@ -0,0 +1,46 @@
+package student.model;
+
+import java.util.List;
+
+/**
+ * Interface to the item model.
+ */
+public interface ItemModel {
+    /** Do not change the file address! */
+    String DATABASE = "data/freegamerecords.json";
+
+    /**
+     * Get the list of items.
+     *
+     * @return the list of items
+     */
+    List<FreeGameItem> getItems();
+
+    /**
+     * Add an item to the list.
+     *
+     * @param item the item to add
+     */
+    void addItem(FreeGameItem item);
+
+    /**
+     * Remove an item from the list.
+     *
+     * @param item the item to remove
+     */
+    void removeItem(FreeGameItem item);
+
+    /**
+     * Save the list to a file.
+     *
+     * @param filePath the path to save the file to
+     */
+    void saveList(String filePath);
+
+    /**
+     * Load the list from a file.
+     *
+     * @param filePath the path to load the file from
+     */
+    void loadList(String filePath);
+}
--- src/main/java/student/model/net/NetUtils.java
+++ src/main/java/student/model/net/NetUtils.java
@@ -4,10 +4,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * A series of classes to help with pulling data from https://www.freetogame.com.
- * 
+ *
  * You can read more about the API at https://www.freetogame.com/api.
  */
 public class NetUtils {
@@ -15,6 +17,13 @@ public class NetUtils {
      * Base URL for the API request.
      */
     private static final String BASE_URL = "https://www.freetogame.com/api";
+
+    /**
+     * Prevent instantiation.
+     */
+    private NetUtils() {
+        // Prevent instantiation
+    }
 
     /**
      * Retrieves a list of all free-to-play games from the FreeToGame API.
@@ -40,7 +49,7 @@ public class NetUtils {
             content.append(inputLine);
         }
 
-        // Close the BufferReader
+        // Close the BufferedReader
         in.close();
 
         // Disconnect the HTTP connection
@@ -51,14 +60,30 @@ public class NetUtils {
     }
 
     /**
-     * Prevent instantiation.
+     * Searches for a game by its name in the list of free-to-play games.
+     *
+     * @param gameName the name of the game to search for
+     * @return a JSONObject containing the game details, or null if the game is not found
+     * @throws Exception if an error occurs during JSON parsing
      */
-    private NetUtils() {
-        // Prevent instantiation
+    public static JSONObject searchGameByName(String gameName) throws Exception {
+
+        // Fetch the list of games from the API
+        String gamesListJson = getGamesList();
+        JSONArray gamesArray = new JSONArray(gamesListJson);
+
+        // Search for the game by name in the games array
+        for (int i = 0; i < gamesArray.length(); i++) {
+            JSONObject game = gamesArray.getJSONObject(i);
+            if (game.getString("title").equalsIgnoreCase(gameName)) {
+                return game; // Return the game details if found
+            }
+        }
+        return null; // Return null if the game is not found
     }
 
     /**
-     * Main method for testing the list obtained from the api.
+     * Main method for testing the list obtained from the API and searching for a specific game.
      *
      * @param args the command line arguments
      */
@@ -67,6 +92,29 @@ public class NetUtils {
             String gamesList = getGamesList();
             System.out.println("Games List: ");
             System.out.println(gamesList);
+
+            // Test searching for a game by name
+            String gameName = "Tibia";
+            JSONObject gameDetails = searchGameByName(gameName);
+
+            if (gameDetails != null) {
+                System.out.println("Game Details for " + gameName + ": ");
+                System.out.println(gameDetails.toString(2)); // Pretty print with an indentation of 2
+            } else {
+                System.out.println("Game " + gameName + " not found.");
+            }
+
+            // Test searching for a game by name "Fortnite"
+            String gameName2 = "Fortnite";
+            JSONObject gameDetails2 = searchGameByName(gameName2);
+
+            if (gameDetails2 != null) {
+                System.out.println("Game Details for " + gameName2 + ": ");
+                System.out.println(gameDetails2.toString(2)); // Pretty print with an indentation of 2
+            } else {
+                System.out.println("Game " + gameName2 + " not found.");
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
