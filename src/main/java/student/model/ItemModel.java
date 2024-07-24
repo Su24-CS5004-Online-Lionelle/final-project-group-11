45b461511366046d189bc79bec12fdcd0ab80527
HarishNandhaKumar <harishkumar2171@gmail.com>
2024-07-24 12:46:34 -0400

    fix: deleted the GamesRecord record

--- src/main/java/student/model/ItemModel.java
+++ src/main/java/student/model/ItemModel.java
@@ -1,11 +1,5 @@
 package student.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-
 import java.util.List;
 
 /**
@@ -20,7 +14,7 @@ public interface ItemModel {
      *
      * @return the list of items
      */
-    List<ItemModel.GamesRecord> getItems();
+    List<FreeGameItem> getItems();
 
     /**
      * Add an item to the list.
@@ -49,28 +43,4 @@ public interface ItemModel {
      * @param filePath the path to load the file from
      */
     void loadList(String filePath);
-
-    /**
-     * Primary record to pass around between objects.
-     * @param id                  the unique identifier of the game item
-     * @param title               the title of the game
-     * @param thumbnail           the URL of the game's thumbnail image
-     * @param shortDescription    a brief description of the game
-     * @param gameUrl             the URL to access the game
-     * @param genre               the genre of the game
-     * @param platform            the platform on which the game can be played
-     * @param publisher           the publisher of the game
-     * @param developer           the developer of the game
-     * @param releaseDate         the release date of the game
-     * @param freetogameProfileUrl the URL to the game's profile on the FreeToGame website
-     */
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @JacksonXmlRootElement(localName = "Games")
-    @JsonPropertyOrder({"id", "title", "thumbnail", "short_description", "game_url", "genre", "platform",
-            "publisher", "developer", "release_date", "freetogame_profile_url"})
-    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-    record GamesRecord(int id, String title, String thumbnail, String shortDescription, String gameUrl,
-                       String genre, String platform, String publisher, String developer, String releaseDate,
-                       String freetogameProfileUrl) {
-    }
 }
