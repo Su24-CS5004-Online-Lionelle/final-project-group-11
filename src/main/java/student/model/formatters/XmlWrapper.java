775c57a3c5d557d5145e4328fafca2c19dcc5567
HarishNandhaKumar <harishkumar2171@gmail.com>
2024-07-24 00:18:27 -0400

    feat: xml wrapper class.

---
+++ src/main/java/student/model/formatters/XmlWrapper.java
@@ -0,0 +1,33 @@
+package student.model.formatters;
+
+import java.util.Collection;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import student.model.ItemModel.GamesRecord;
+
+/**
+ * This wrapper helps when using Jackson to serialize a list of domain records to xml.
+ * Without this, it tries to use <ArrayList> and <item> tags instead of <domainList> and <domain> tags.
+ * Suggested use (note you need try/catch with this)
+ * <pre>
+ * XmlMapper mapper = new XmlMapper();
+ * mapper.enable(SerializationFeature.INDENT_OUTPUT);
+ * DomainXmlWrapper wrapper = new DomainXmlWrapper(records);
+ * mapper.writeValue(out, wrapper);
+ * </pre>
+ */
+@JacksonXmlRootElement(localName = "GamesList")
+public final class XmlWrapper {
+
+    /** List of the records. */
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private Collection<GamesRecord> games;
+
+    /**
+     * Constructor.
+     * @param games the records to wrap
+     */
+    public XmlWrapper(Collection<GamesRecord> games) {
+        this.games = games;
+    }
+}
