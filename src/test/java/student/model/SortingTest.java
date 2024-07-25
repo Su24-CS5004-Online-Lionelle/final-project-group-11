11793554c12ed0732e7f644f1f4cf324d67b3608
lingliu <liuling201030@gmail.com>
2024-07-25 16:43:39 -0400

    update sorting test

--- src/test/java/student/model/SortingTest.java
+++ src/test/java/student/model/SortingTest.java
@@ -13,25 +13,49 @@ public class SortingTest {
     private List<FreeGameItem> items;
 
     @BeforeEach
-    void setup(){
+    public void setUp() {
         items = new ArrayList<>();
         items.add(new FreeGameItem(1, "Game One", "url1", "Desc1", "url1", "Action", "PC", "Publisher1", "Developer1", "2024-01-01", "url1"));
-        items.add(new FreeGameItem(2, "Game Two", "url2", "Desc2", "url2", "Adventure", "Console", "Publisher2", "Developer2", "2023-01-02", "url2"));
+        items.add(new FreeGameItem(2, "Game Two", "url2", "Desc2", "url2", "Adventure", "Console", "Publisher2", "Developer2", "2024-01-02", "url2"));
+        items.add(new FreeGameItem(3, "Another Game", "url3", "Desc3", "url3", "Puzzle", "Mobile", "Publisher3", "Developer3", "2024-01-03", "url3"));
     }
 
     @Test
-    public void testSortItemsByTitle() {
+    public void testSortItemsByTitleAscending() {
         List<FreeGameItem> sortedItems = Sorting.sortItems(items, "title", true);
-
-        assertEquals("Game One", sortedItems.get(0).getTitle());
-        assertEquals("Game Two", sortedItems.get(1).getTitle());
+        assertEquals("Another Game", sortedItems.get(0).getTitle());
+        assertEquals("Game One", sortedItems.get(1).getTitle());
+        assertEquals("Game Two", sortedItems.get(2).getTitle());
     }
 
     @Test
-    public void testSortItemByGenre() {
-        List<FreeGameItem> sortedItems = Sorting.sortItems(items, "genre", true);
+    public void testSortItemsByTitleDescending() {
+        List<FreeGameItem> sortedItems = Sorting.sortItems(items, "title", false);
+        assertEquals("Game Two", sortedItems.get(0).getTitle());
+        assertEquals("Game One", sortedItems.get(1).getTitle());
+        assertEquals("Another Game", sortedItems.get(2).getTitle());
+    }
 
-        assertEquals("Action", sortedItems.get(0).getGenre());
-        assertEquals("Adventure", sortedItems.get(1).getGenre());
+    @Test
+    public void testSortItemsByReleaseDateAscending() {
+        List<FreeGameItem> sortedItems = Sorting.sortItems(items, "releasedate", true);
+        assertEquals("2024-01-01", sortedItems.get(0).getReleaseDate());
+        assertEquals("2024-01-02", sortedItems.get(1).getReleaseDate());
+        assertEquals("2024-01-03", sortedItems.get(2).getReleaseDate());
+    }
+
+    @Test
+    public void testSortItemsByReleaseDateDescending() {
+        List<FreeGameItem> sortedItems = Sorting.sortItems(items, "releasedate", false);
+        assertEquals("2024-01-03", sortedItems.get(0).getReleaseDate());
+        assertEquals("2024-01-02", sortedItems.get(1).getReleaseDate());
+        assertEquals("2024-01-01", sortedItems.get(2).getReleaseDate());
+    }
+
+    @Test
+    public void testUnsupportedSortField() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Sorting.sortItems(items, "unsupportedField", true);
+        });
     }
 }
