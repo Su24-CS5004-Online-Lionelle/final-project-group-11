68cea873ad52b9653a6c8ab939822145e50ae9fb
HarishNandhaKumar <harishkumar2171@gmail.com>
2024-07-28 00:39:41 -0400

    feat: Controller gets the instance of model and view class gets the instance of controller.

--- src/main/java/student/MainApp.java
+++ src/main/java/student/MainApp.java
@@ -1,6 +1,9 @@
 package student;
 
+import student.controller.Controller;
+import student.model.ItemModelImpl;
 import student.view.MainView;
+
 
 public class MainApp {
 
@@ -8,13 +11,14 @@ public class MainApp {
     private MainApp() {
         //empty
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         /*
          * feel free to delete following line 
          * just a test for GUI setup
          */
         System.out.println("to set up gradle project, run: gradle init");
-        MainView mainView = new MainView();
+        Controller obj = new Controller(new ItemModelImpl());
+        MainView mainView = new MainView(obj);
         mainView.setVisible(true);
     }
     
