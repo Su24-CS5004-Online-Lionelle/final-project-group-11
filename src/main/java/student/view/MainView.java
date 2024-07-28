36c4d135d58d60b425e7e6dbe25b1fa37cd09734
HarishNandhaKumar <harishkumar2171@gmail.com>
2024-07-28 00:37:42 -0400

    feat: Added the listener methods for search, add, remove and list buttons for single search function.

--- src/main/java/student/view/MainView.java
+++ src/main/java/student/view/MainView.java
@@ -1,9 +1,13 @@
 package student.view;
 
-import java.awt.BorderLayout;
+import student.controller.Controller;
+import student.model.FreeGameItem;
+import student.model.ItemModelImpl;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
 
 /**
  * MainView is a JFrame that combines various views into a single window.
@@ -15,13 +19,15 @@ public class MainView extends JFrame {
     private SortFilterView sortFilterView;
     private ResultDisplayView resultDisplayView;
     private ExportButtonView exportButtonView;
+    private Controller controller;
 
     /**
      * Constructs a new MainView.
      * Sets the title, default close operation, size, and layout of the frame.
      * Initializes and adds the various panels to the frame.
      */
-    public MainView() {
+    public MainView(Controller obj) {
+        this.controller = obj;
         setTitle("Free Game Library");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 800);
@@ -41,5 +47,51 @@ public class MainView extends JFrame {
         add(topPanel, BorderLayout.NORTH);
         add(resultDisplayView, BorderLayout.CENTER);
         add(exportButtonView, BorderLayout.SOUTH);
+
+        searchAddRemoveView.setSearchListener(this::searchButtonListener);
+        searchAddRemoveView.setAddListener(this::addButtonListener);
+        searchAddRemoveView.setRemoveListener(this::removeButtonListener);
+        sortFilterView.setListButtonListener(this::listButtonListener);
+
+    }
+
+    /**
+     * This method is the listener for the Search button.
+     * @param e the action event is taken as input.
+     */
+    private void searchButtonListener(ActionEvent e) {
+        String hostname = commandInputView.getCommandText();
+        if (hostname == null || hostname.isEmpty()) {
+            resultDisplayView.setResultText("Nothing to search");
+        }
+        else {
+            resultDisplayView.setResultText(this.controller.getSingleGame(hostname.toLowerCase()));
+        }
+    }
+
+    /**
+     * This method is the listener for the Add button.
+     * @param e the action event is taken as input.
+     */
+    private void addButtonListener(ActionEvent e) {
+        this.controller.AddGameToList();
+    }
+
+    /**
+     * This method is the listener for the List button.
+     * @param e the action event is taken as input.
+     */
+    private void listButtonListener(ActionEvent e) {
+        resultDisplayView.setResultText(this.controller.getAllGamesList());
+    }
+
+    /**
+     * This method is the listener for the Remove button.
+     * @param e the action event is taken as input.
+     */
+    private void removeButtonListener(ActionEvent e) {
+        String input = JOptionPane.showInputDialog(searchAddRemoveView.getRemoveButton(),
+                "Enter the Game Name to remove:", "Remove Game", JOptionPane.PLAIN_MESSAGE);
+        this.controller.removeGameFromList(input.toLowerCase());
     }
 }
