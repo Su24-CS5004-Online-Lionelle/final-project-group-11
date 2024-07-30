3bd898fb26cebfd0059bea254a704e524408b546
HarishNandhaKumar <harishkumar2171@gmail.com>
2024-07-30 00:54:57 -0400

    feat: Implemented the sort button, filter button, add all button and list button.

--- src/main/java/student/view/MainView.java
+++ src/main/java/student/view/MainView.java
@@ -1,8 +1,7 @@
 package student.view;
 
 import student.controller.Controller;
-import student.model.FreeGameItem;
-import student.model.ItemModelImpl;
+import student.model.formatters.Formats;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
@@ -53,7 +52,8 @@ public class MainView extends JFrame {
         searchAddRemoveView.setRemoveListener(this::removeButtonListener);
         sortFilterView.setListButtonListener(this::listButtonListener);
         sortFilterView.setFilterButtonListener(this::filterButtonListener);
-
+        sortFilterView.setSortButtonListener(this::sortButtonListener);
+        sortFilterView.setAddAllButtonListener(this::addAllButtonListener);
     }
 
     /**
@@ -63,7 +63,7 @@ public class MainView extends JFrame {
     private void searchButtonListener(ActionEvent e) {
         String hostname = commandInputView.getCommandText();
         if (hostname == null || hostname.isEmpty()) {
-            resultDisplayView.setResultText("Nothing to search");
+            resultDisplayView.setResultText("Nothing to Search");
         }
         else {
             resultDisplayView.setResultText(this.controller.getSingleGame(hostname));
@@ -77,9 +77,11 @@ public class MainView extends JFrame {
     private void addButtonListener(ActionEvent e) {
         String input = JOptionPane.showInputDialog(searchAddRemoveView.getRemoveButton(),
                 "Enter the Game Name to Add:", "Add Game", JOptionPane.PLAIN_MESSAGE);
-        if (!input.isEmpty() || input == null) {
-            this.controller.addGameToList(input);
+        if (input == null || input.isEmpty()) {
+            resultDisplayView.setResultText("Nothing to Add");
+            return;
         }
+        this.controller.addGameToList(input);
     }
 
     /**
@@ -87,7 +89,16 @@ public class MainView extends JFrame {
      * @param e the action event is taken as input.
      */
     private void listButtonListener(ActionEvent e) {
-        resultDisplayView.setResultText(this.controller.getAllGamesList());
+        String[] options = {"JSON", "XML", "CSV", "PRETTY"};
+        String input = (String) JOptionPane.showInputDialog(sortFilterView.getFilterButton(),
+                "Choose the Format to Display:", "Formats Menu", JOptionPane.QUESTION_MESSAGE,
+                null, options, options[0]);
+        switch (input) {
+            case "JSON" -> resultDisplayView.setResultText(this.controller.getAllGamesList(Formats.JSON));
+            case "XML" -> resultDisplayView.setResultText(this.controller.getAllGamesList(Formats.XML));
+            case "CSV" -> resultDisplayView.setResultText(this.controller.getAllGamesList(Formats.CSV));
+            case "PRETTY" -> resultDisplayView.setResultText(this.controller.getAllGamesList(Formats.PRETTY));
+        }
     }
 
     /**
@@ -97,35 +108,69 @@ public class MainView extends JFrame {
     private void removeButtonListener(ActionEvent e) {
         String input = JOptionPane.showInputDialog(searchAddRemoveView.getRemoveButton(),
                 "Enter the Game Name to remove:", "Remove Game", JOptionPane.PLAIN_MESSAGE);
-        this.controller.removeGameFromList(input);
+        resultDisplayView.setResultText(this.controller.removeGameFromList(input));
     }
 
+    /**
+     * This method is the listener for the Filter button.
+     * @param e the action event is taken as input.
+     */
     private void filterButtonListener(ActionEvent e) {
         String[] options = {"id", "title", "thumbnail", "short_description", "game_url",
-                "genre", "platform", "publisher", "developer", "release_date", "NONE"};
-
+                "genre", "platform", "publisher", "developer", "release_date"};
         String selectedValue = (String) JOptionPane.showInputDialog(sortFilterView.getFilterButton(),
                 "Select a filter Parameter:", "Filter Parameter Menu", JOptionPane.QUESTION_MESSAGE,
                 null, options, options[0]);
 
-        if (selectedValue != null) {
-            resultDisplayView.setResultText("Filter Parameter: " + selectedValue);
-        } else {
-            resultDisplayView.setResultText("Filter Parameter: None");
-        }
-
-        String[] options2 = {"EQUALS", "CONTAINS", "NOT_EQUAL", "GREATER_THAN", "LESS_THAN",
-                "GREATER_EQUAL", "LESS_EQUAL", "NONE"};
-
+        String[] options2 = {"==", "~=", "!=", ">", "<", ">=", "<="};
         String selectedValue2 = (String) JOptionPane.showInputDialog(sortFilterView.getFilterButton(),
                 "Select a filter Type:", "Filter Type Menu", JOptionPane.QUESTION_MESSAGE,
                 null, options2, options2[0]);
 
-        if (selectedValue2 != null) {
-            resultDisplayView.setResultText("Filter Parameter: " + selectedValue + "\n" +
-                            "Filter Type: " + selectedValue2);
+        String parameterValue = JOptionPane.showInputDialog(searchAddRemoveView.getRemoveButton(),
+                "Enter the value:", "Filter Value", JOptionPane.PLAIN_MESSAGE);
+        if (selectedValue != null && selectedValue2 != null && parameterValue != null) {
+            String fullString = selectedValue + selectedValue2 + parameterValue;
+            resultDisplayView.setResultText(this.controller.filterGames(fullString));
         } else {
-            resultDisplayView.setResultText("Filter Parameter: None");
+            resultDisplayView.setResultText("Filter Incomplete");
         }
+    }
+
+    /**
+     * This method is the listener for the Sort button.
+     * @param e the action event is taken as input.
+     */
+    private void sortButtonListener(ActionEvent e) {
+        String[] options = {"title", "genre", "publisher", "developer", "release_date"};
+
+        String selectedValue = (String) JOptionPane.showInputDialog(sortFilterView.getFilterButton(),
+                "Select a Sort Parameter:", "Sort Parameter Menu", JOptionPane.QUESTION_MESSAGE,
+                null, options, options[0]);
+
+        String[] options2 = {"Ascending", "Descending"};
+        String selectedValue2 = (String) JOptionPane.showInputDialog(sortFilterView.getFilterButton(),
+                "Select a Sort Type:", "Sort Type Menu", JOptionPane.QUESTION_MESSAGE,
+                null, options2, options2[0]);
+
+        if (selectedValue == null || selectedValue2 == null) {
+            resultDisplayView.setResultText("Sort option is null");
+            return;
+        }
+
+        boolean choice = true;
+        if (selectedValue2.equals("Descending")) {
+            choice = false;
+        }
+
+        resultDisplayView.setResultText(this.controller.sortGames(selectedValue, choice));
+    }
+
+    /**
+     * This method is the listener for the Add All button.
+     * @param e the action event is taken as input.
+     */
+    private void addAllButtonListener(ActionEvent e) {
+        resultDisplayView.setResultText(this.controller.addAllGamesToList());
     }
 }
